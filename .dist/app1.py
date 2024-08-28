6b595dfa4bb08bfcb630e59a303871f348ce7def
sivakavineshnattudurai <kavinesh342003@gmail.com>
2024-08-28 12:04:33 +0530

    File imported

---
+++ .dist/app1.py
@@ -0,0 +1,8 @@
+from flask import flask, render_template, request, url_for
+from flask_bootstrap import Bootstrap 
+from textblob import TextBlob, word
+import random
+import time
+
+app1=Flask(__name__)
+Bootstrap(app1)
