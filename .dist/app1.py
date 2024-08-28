4330ee90b6bcb55e439dbdc5ed58d9c1ae6652a8
sivakavineshnattudurai <kavinesh342003@gmail.com>
2024-08-28 12:06:23 +0530

    file added

--- .dist/app1.py
+++ .dist/app1.py
@@ -2,7 +2,7 @@ from flask import flask, render_template, request, url_for
 from flask_bootstrap import Bootstrap 
 from textblob import TextBlob, word
 import random
-import time
+import time 
 
 app1=Flask(__name__)
 Bootstrap(app1)
